515fc1d2dd18996b3c4c245274793c97cfe997f4
Relise228 <andreysmit43@gmail.com>
2020-04-06 11:51:30 +0300

    Add review

--- client/src/components/OrderBox/OrderBox.jsx
+++ client/src/components/OrderBox/OrderBox.jsx
@@ -46,7 +46,7 @@ export class OrderBox extends React.Component {
                     <div className="order_date">{this.dateSTR.getDate() + "." + (this.dateSTR.getMonth() + 1) + '.' + this.dateSTR.getUTCFullYear() + 
                     ' ' + this.dateSTR.getHours() + ':' + this.dateSTR.getMinutes()}</div>
                     <div className="order_total-price">{this.state.totalPrice + " грн"} </div>
-                    <div className="order_status" style={this.state.status == "Confirmed" ? ConfirmedOrder :  WaitingOrder}>{this.state.status} </div>
+                    <div className="order_status" style={this.state.status === "Confirmed" ? ConfirmedOrder :  WaitingOrder}>{this.state.status} </div>
                 </div>
                 <div className="order_full">
                     {this.props.order.products.map(product => (
